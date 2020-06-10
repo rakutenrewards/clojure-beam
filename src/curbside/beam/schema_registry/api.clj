3460c0cb0775f7331dfe061af8299fa131d23d8b
Mathieu <mathieu.carpentier@rakuten.com>
2020-06-10 11:25:09 -0400

    Add schema registry authentication

--- src/curbside/beam/schema_registry/api.clj
+++ src/curbside/beam/schema_registry/api.clj
@@ -1,8 +1,11 @@
 (ns curbside.beam.schema-registry.api
+  (:require [clojure.tools.logging :as log])
   (:import
-   (io.confluent.kafka.schemaregistry.client CachedSchemaRegistryClient)))
+   (io.confluent.kafka.schemaregistry.client CachedSchemaRegistryClient)
+   (io.confluent.kafka.schemaregistry.client.rest RestService)))
 
-(defn- make-client- [{:keys [url cache-capacity] :as _schema-registry-cfg}]
-  (CachedSchemaRegistryClient. ^String url ^int cache-capacity))
+(defn- make-client- [{:keys [url cache-capacity auth] :as _schema-registry-cfg}]
+  (let [restClient (RestService. ^String url)]
+    (CachedSchemaRegistryClient. ^RestService restClient ^int cache-capacity auth)))
 
 (def make-client (memoize make-client-))
